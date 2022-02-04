7f969bc578de5071ab8a97ee59e2c30fea330ad4
Timma1991 <97384175+Timma1991@users.noreply.github.com>
2022-02-04 18:54:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Massiv4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] students =
+{
+    { "X" ,"O","X" ,"O","X" ,"O","X" ,"O","X" ,"O", },
+    { "X" ,"O","X" ,"O","X" ,"O","X" ,"O","X" ,"O", },
+    { "X" ,"O","X" ,"O","X" ,"O","X" ,"O","X" ,"O", },
+    { "X" ,"O","X" ,"O","X" ,"O","X" ,"O","X" ,"O", },
+    { "X" ,"O","X" ,"O","X" ,"O","X" ,"O","X" ,"O", },
+    { "X" ,"O","X" ,"O","X" ,"O","X" ,"O","X" ,"O", },
+    { "X" ,"O","X" ,"O","X" ,"O","X" ,"O","X" ,"O", },
+     { "X" ,"O","X" ,"O","X" ,"O","X" ,"O","X" ,"O", },
+      { "X" ,"O","X" ,"O","X" ,"O","X" ,"O","X" ,"O", },
+       { "X" ,"O","X" ,"O","X" ,"O","X" ,"O","X" ,"O", },
+};
+
+            int heigth = students.GetLength(0);
+
+            int width = students.GetLength(1);
+
+            int students1 = 0;
+            for (int y = 0; y < heigth; y++)
+            {
+                string currentValue = students[y, students1];
+
+                for (int i = 0; i < width; i++)
+                {
+                    Console.Write(students[y, i]);
+                }
+                Console.WriteLine();
+            }
+    }
+}
