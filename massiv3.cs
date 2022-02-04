ccc783bf2d926a805758aeaf075f5fa8b9f9c4f4
Timma1991 <97384175+Timma1991@users.noreply.github.com>
2022-02-04 19:21:47 +0300

    Add files via upload

---
+++ massiv3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Massiv3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name =
+{
+    "",
+    
+};
+
+            Console.Write("Введите имя:");
+            
+            string name1 = Console.ReadLine();
+
+
+            for (int i = name1.Length - 1; i >= 0; i--)
+            {
+                Console.Write(name1[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
