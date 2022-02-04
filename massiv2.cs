421c0fc9a09732a5e6e1a38fedaa5d914ce8724d
Timma1991 <97384175+Timma1991@users.noreply.github.com>
2022-02-04 19:21:08 +0300

    Add files via upload

---
+++ massiv2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string[,] students =
+{
+    { "Иванов" ,"453636" },
+    { "Петров" ,"634636" },
+    { "Сидоров","3462346" },
+    { "Петрова","34634"},
+    {"Филиппова","346347"},
+    {"Егоров" ,"3463246"},
+    { "Козлова","346346"},
+};
+            
+            int heigth = students.GetLength(0);
+           
+            int width = students.GetLength(1);
+
+            int students1 = 0;
+            for (int y = 0; y < heigth; y++)
+            {
+                string currentValue = students[y,students1];
+
+                for (int i = 0; i < width; i++)
+                {
+                    Console.Write(students[y,i]);
+                }
+                Console.WriteLine();
+            }
+    }
+}
